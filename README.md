2e88fbb0206da6eb1084ee931f414c9ab8dd09ee
NewUser <nollyenterprise@gmail.com>
2023-06-14 09:30:23 +0100

    Commit 2023-06-14 09:00:00

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # traffic-light
 This is a traffic light designed with CSS and the funtionalities controlled with javascript. the light are programmed to switch on and off at certain intervals. 
+
 1. design the traffic light to be on top of three bottons.(it is controlled with three buttons).
 
-2.the first botton is to stop all functions that is running the traffic light. when the first button is clicked all the lights go off and the value of the input screen is emptied. 
+2. the first button is to stop all functions that is running the traffic light. when the first button is clicked all the lights go off and the value of the input screen is emptied. 
 
 3. the second button is to start the traffic light.
 
-4.the third button is also to start the traffic light. 
+4. the third button is also to start the traffic light. 
 
 5. add an input screen that shows the count down. 
 
@@ -17,14 +18,14 @@ the speed should be 1 second per tick,
 
 8. when the third button is clicked and the count down was previously started for the second button.
 
-8a. if the count down is on red, only increase the speed from one seconds to three seconds.
+    - if the count down is on red, only increase the speed from one seconds to three seconds.
 
-8b. if the count down is on yellow, only increase the speed from one second to 3 seconds
+    - if the count down is on yellow, only increase the speed from one second to 3 seconds
 
 9. when the second button is clicked and the count down was previously started for the third button.
 
-9a. if the count down is on red, only decrease the speed from three seconds to one seconds.
+    - if the count down is on red, only decrease the speed from three seconds to one seconds.
 
-9b. if the count down is on yellow, only decrease the speed from three second to one seconds
+    - if the count down is on yellow, only decrease the speed from three second to one seconds
 
 10. if the count down is equals to the 0 and on green, do nothing when the second or the third button is clicked. if you want to start the count down again you must click the first button to reset. 
--- script.js
+++ script.js
@@ -1,83 +1,202 @@
-const red = document.querySelector(".red");
-const yellow = document.querySelector(".yellow");
-const green = document.querySelector(".green");
+let countDown = null;
+let currentColor = null;
+let isStarted = false; // Boolean
+let countDownVariable;
+let startValue;
 
-const clickOne = document.querySelector(".first-start");
-const clickTwo = document.querySelector(".second-start");
-const stopper = document.querySelector(".stopper")
-let countdownDisplay = document.querySelector("#input"); // HTML element to display the countdown
-let countdownDuration = 60; // Countdown duration in seconds
-
-
-
-//function to change light colors
-const redLight = () => {
-    red.classList.add('active')
-    setTimeout(() => {
-        red.classList.remove('active')
-    }, 30000);
+// COLORS INITIAL DECLARATIONS
+let color = {
+    red: {
+        secondButton: {
+            start: {
+                isCountDown: true,
+                start: 30,
+                speed: 1 // Seconds
+            }
+        },
+        thirdButton: {
+            start: {
+                isCountDown: true,
+                start: 50,
+                speed: 0.1 // Seconds
+            }
+        },
+    },
+    yellow: {
+        secondButton: {
+            start: {
+                isCountDown: true,
+                start: 8,
+                speed: 1 // Seconds
+            }
+        },
+        thirdButton: {
+            start: {
+                isCountDown: true,
+                start: 30,
+                speed: 0.1 // Seconds
+            }
+        },
+    },
+    green: {
+        secondButton: {
+            start: {
+                isCountDown: false,
+                start: 0,
+                speed: 1 // Seconds
+            }
+        },
+        thirdButton: {
+            start: {
+                isCountDown: false,
+                start: 0,
+                speed: 0.1 // Seconds
+            }
+        },
+    }
 }
-const yellowLight = () => {
-    yellow.classList.add('active')
-    setTimeout(() => {
-        yellow.classList.remove('active')
-    }, 8000);
-}
-const greenLight = () => {
-    green.classList.add('active')
-    setTimeout(() => {
-        green.classList.remove('active')
-    }, 10000);
 
+
+const startTrafficLight = (start, speed, color, button) => {
+    document.querySelectorAll(`.tLight>div`)
+    .forEach((v)=>v.classList.remove("active"));
+    setTimeout(() => {
+        document.querySelector(`.tLight>div.${color}`).classList.add("active");
+    }, speed);
+    startCountDown (start, speed, color, button);
 }
 
+const switchTrafficLightButton = (start, speed, color, button) => {
+    document.querySelectorAll(`.tLight>div`)
+    .forEach((v)=>v.classList.remove("active"));
+    setTimeout(() => {
+        document.querySelector(`.tLight>div.${color}`).classList.add("active");
+    }, speed);
+    clearInterval(countDownVariable);
+    startCountDown (start, speed, color, button);
+}
 
-function countdownTimer(duration, inputElement) {
-    let timer = duration, seconds;
+const nextCountDown = (colorList, button) => {
+    if(colorList == "red"){
+        currentColor = "yellow";
+    } else {
+        currentColor = "green";
+    }
+    isStarted = true;
+    let isCountDown = color[currentColor][button+"Button"].start.isCountDown;
+    let start = color[currentColor][button+"Button"].start.start;
+    let speed = color[currentColor][button+"Button"].start.speed;
 
-    let countdownInterval = setInterval(function () {
-        seconds = Math.floor(timer % 60);
+    clearInterval (countDownVariable);
+    if (currentColor == "green") {
+        document.querySelectorAll(`.tLight>div`)
+        .forEach((v)=>v.classList.remove("active"));
+        document.querySelector(`.tLight>div.${currentColor}`).classList.add("active");
+        toScreen (0);
+    } else {
+        if(isCountDown === true)
+            startTrafficLight (start, speed, currentColor, button);
+    }
+}
 
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        inputElement.value = seconds;
-
-        if (--timer < 0) {
-            clearInterval(countdownInterval);
+const startCountDown = (start, speed, color, button) => {
+    startValue = start;
+    countDownVariable = setInterval(() => {
+        toScreen (startValue);
+        if(startValue == 1) {
+            nextCountDown (color, button);
         }
-    }, 1000);
+        startValue = (parseFloat(startValue) - 1);
+    }, (parseFloat(speed)*1000));
 }
 
-
-countdownTimer(countdownDuration, countdownDisplay);
-
-
-//function call to switc lite on at click of first button.
-const myFirstButton = () => {
-    setTimeout(() => {
-        redLight()
-    }, 0);
-    setTimeout(() => {
-        yellowLight()
-    }, 30000);
-    setTimeout(() => {
-        greenLight()
-    }, 38000);
+const toScreen = (v) => {
+    document.querySelector("div.count>input").value = v;
 }
 
+const stopFunction = () => {
+    document.querySelectorAll(`.tLight>div`)
+    .forEach((v)=>v.classList.remove("active"));
+    clearInterval (countDownVariable);
+    toScreen ("");
+    countDown = null;
+    currentColor = null;
+    isStarted = false; 
+}
 
+const secondStartFunction = () => {
+    
+    // If countdown is started ===============
+    if(isStarted) {
 
+        if(currentColor){
+            let isCountDown = color[currentColor].secondButton.start.isCountDown;
+            let start = startValue;
+            let speed = color[currentColor].secondButton.start.speed;
+            if(isCountDown === true){
+                switchTrafficLightButton (start, speed, currentColor, "second");
+            }
+        }
 
+    // If countdown is not started ===============
+    } else {
+        isStarted = true;
+        if(currentColor == null){
+            currentColor = "red";
+            let isCountDown = color[currentColor].secondButton.start.isCountDown;
+            let start = color[currentColor].secondButton.start.start;
+            let speed = color[currentColor].secondButton.start.speed;
+            if(isCountDown === true){
+                startTrafficLight (start, speed, currentColor, "second");
+            }
+        }
+    }
+}
+const thirdStartFunction = () => {
+    
+    // If countdown is started ===============
+    if(isStarted) {
 
-clickOne.addEventListener("click", myFirstButton(
+        if(currentColor){
+            let isCountDown = color[currentColor].thirdButton.start.isCountDown;
+            let start = startValue;
+            let speed = color[currentColor].thirdButton.start.speed;
+            if(isCountDown === true){
+                switchTrafficLightButton (start, speed, currentColor, "third");
+            }
+        }
 
-));
-//clickTwo.addEventListener("click", mySecondButton);
-//stopper.addEventListener("click", stopAllLight);
+    // If countdown is not started ===============
+    } else {
+        isStarted = true;
+        if(currentColor == null){
+            currentColor = "red";
+            let isCountDown = color[currentColor].thirdButton.start.isCountDown;
+            let start = color[currentColor].thirdButton.start.start;
+            let speed = color[currentColor].thirdButton.start.speed;
+            if(isCountDown === true){
+                startTrafficLight (start, speed, currentColor, "third");
+            }
+        }
+    }
+}
 
-
-
-
-
-
-
+let getButtons = document.querySelectorAll(".buttons>button");
+getButtons.forEach( (v) => {
+    v.onclick = (v) => {
+        let class_ = v.target.className;
+        switch (class_) {
+            case "stopper":
+                stopFunction ();
+                break;
+            case "second-start":
+                secondStartFunction ();
+                break;
+            case "third-start":
+                thirdStartFunction ();
+                break;
+            default:
+                break;
+        }
+    }
+})
